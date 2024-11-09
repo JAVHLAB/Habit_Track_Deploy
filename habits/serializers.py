a06c0c346e2ec0d410343c5c0f61da34469273c3
unknown <francisco.mariscal4331@alumnos.udg.mx>
2024-11-09 15:26:15 -0600

    Contraseña se establece pero no sera visible desde /api/usuarios/

--- habits/serializers.py
+++ habits/serializers.py
@@ -2,9 +2,26 @@ from rest_framework import serializers
 from .models import Usuario, Habito, Notificacion, Nota, Ejecucion, Estadisticas
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = Usuario
-        fields = ['id', 'username', 'email', 'foto_perfil', 'fecha_registro']
+        fields = ['id', 'username', 'email', 'foto_perfil', 'fecha_registro','password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = Usuario(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class HabitoSerializer(serializers.ModelSerializer):
     class Meta:
